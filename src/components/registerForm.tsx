73e6bd7934d6b7c05825aa673cc2e050ba832ec8
kishanshettyb <kishanqr@gmail.com>
2024-08-31 10:23:25 +0530

    feat: add modal component and registerform component

---
+++ src/components/registerForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {};
+
+function RegisterForm({}: Props) {
+  return (
+    <div>
+      RegisterForm Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta iusto fugit
+      quae perspiciatis, sapiente iste! Vitae temporibus deserunt velit laboriosam consectetur ad
+      adipisci et, impedit reiciendis odio aspernatur iste?
+    </div>
+  );
+}
+
+export default RegisterForm;
